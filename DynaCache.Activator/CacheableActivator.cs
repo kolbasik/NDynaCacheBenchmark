2eed0659072695fbd5fe23dd829eaabc1dd47822
kolbasik <sergei.kolbasin@gmail.com>
2016-05-21 22:57:57 +0200

    CacheableActivator consumes IDynaCacheService instead of MemoryCacheService

--- DynaCache.Activator/CacheableActivator.cs
+++ DynaCache.Activator/CacheableActivator.cs
@@ -17,11 +17,11 @@ namespace DynaCache
             return CreateInstance(new MemoryCacheService(), arguments);
         }
 
-        public static T CreateInstance(MemoryCacheService memoryCacheService, params object[] arguments)
+        public static T CreateInstance(IDynaCacheService dynaCacheService, params object[] arguments)
         {
-            if (memoryCacheService == null) throw new ArgumentNullException(nameof(memoryCacheService));
-            var objects = new object[] {memoryCacheService}.Concat(arguments).ToArray();
-            return (T) System.Activator.CreateInstance(CacheableType, objects);
+            if (dynaCacheService == null) throw new ArgumentNullException(nameof(dynaCacheService));
+            var objects = new object[] {dynaCacheService}.Concat(arguments).ToArray();
+            return (T) Activator.CreateInstance(CacheableType, objects);
         }
     }
 }
